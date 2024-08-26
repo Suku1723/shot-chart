ddff781c22caeb81ec27a40cbaf840c0f362d1b7
Suku1723 <85752977+Suku1723@users.noreply.github.com>
2024-08-26 11:37:18 -0400

    Update players.py

--- players.py
+++ players.py
@@ -16,5 +16,4 @@ def player_list(id):
 
         x_missed = [i[0] for i in x]
         y_missed = [i[1] for i in x]
-        print(x_made, y_made)
         shotchart.shot_chart(x_made, y_made, x_missed, y_missed, selected_player)
